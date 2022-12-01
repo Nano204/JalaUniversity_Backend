9f99553ebb451c5e30b8d8ccc8d18c60b6fe44e3
Nano204 <fgomeztenorio@hotmail.com>
2022-11-30 22:47:23 -0500

    20221130_Class - Generators & Overloads

--- 20221121_Class - Enviorment/project/src/examples.ts
+++ 20221121_Class - Enviorment/project/src/examples.ts
@@ -44,3 +44,29 @@ console.log(test.next());
 console.log(test.next());
 console.log(test.next());
 console.log(test.next());
+
+class Reservation {
+  destination: string | undefined;
+  constructor(private from: Date, private toOrDestination: Date | string) {}
+  setDestination(destination: string) {
+    this.destination = destination;
+  }
+}
+
+type Reserve = {
+  (from: Date, to: Date, destination: string): Reservation;
+  (from: Date, destination: string): Reservation;
+};
+
+const reserve: Reserve = (
+  from: Date,
+  toOrDestination: Date | string,
+  destination?: string
+) => {
+  const newReservation = new Reservation(from, toOrDestination);
+  if (destination) newReservation.setDestination(destination);
+  return newReservation;
+};
+
+console.log(reserve(new Date(), new Date(), "String"));
+console.log(reserve(new Date(), "String"));
